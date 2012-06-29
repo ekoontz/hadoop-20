8317b0427ffc599d667a67ccdfaac39505b88281
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-29 12:42:51 -0700

    fix hadoop version to 0.20.1; fix group id to com.facebook.hadoop

--- copy-hdfs-jars-to-maven.sh
+++ copy-hdfs-jars-to-maven.sh
@@ -9,14 +9,14 @@ BASEDIR=`dirname $0`
 cd ${BASEDIR}
 
 if [ ! -f build/hadoop-0.20.1-dev-core.jar ]; then
- if [ ! -f build/hadoop-0.20-core.jar ]; then
+ if [ ! -f build/hadoop-0.20.1-core.jar ]; then
   echo "core jar not found. Running 'ant jar'..."
   ant jar | grep BUILD;
  fi
 fi
 
 if [ ! -f build/hadoop-0.20.1-dev-test.jar ]; then
- if [ ! -f build/hadoop-0.20-test.jar ]; then
+ if [ ! -f build/hadoop-0.20.1-test.jar ]; then
    echo "test jar not found. Running 'ant jar-test'..."
    ant jar-test | grep BUILD;
  fi
@@ -32,7 +32,7 @@ fi
 if [ -f build/hadoop-0.20.1-dev-core.jar ]; then
   CORE_JAR=build/hadoop-0.20.1-dev-core.jar
 else
-  CORE_JAR=build/hadoop-0.20-core.jar
+  CORE_JAR=build/hadoop-0.20.1-core.jar
 fi
 
 if [ -f build/hadoop-0.20.1-dev-test.jar ]; then
@@ -48,16 +48,16 @@ echo "** HBase builds will  pick up the HDFS* jars from the local maven repo."
 
 mvn install:install-file \
     -DgeneratePom=true \
-    -DgroupId=org.apache.hadoop \
+    -DgroupId=com.facebook.hadoop \
     -DartifactId=hadoop-core \
-    -Dversion=0.20 \
+    -Dversion=0.20.1 \
     -Dpackaging=jar \
     -Dfile=${CORE_JAR}
 
 mvn install:install-file \
     -DgeneratePom=true \
-    -DgroupId=org.apache.hadoop \
+    -DgroupId=com.facebook.hadoop \
     -DartifactId=hadoop-test \
-    -Dversion=0.20 \
+    -Dversion=0.20.1 \
     -Dpackaging=jar \
     -Dfile=${TEST_JAR}
